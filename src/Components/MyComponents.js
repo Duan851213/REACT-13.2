791b1419d65ba45f47850082dea600b8297aa688
Duan <leeduan8448@gmail.com>
2023-12-13 22:42:53 +0700

    buoi13

---
+++ src/Components/MyComponents.js
@@ -0,0 +1,10 @@
+import React from "react"
+class MyComponents extends React.Component{
+    render(){
+        return(
+            ///JSX
+           <div>eric rat gioi</div>
+        );
+    }
+}
+export default MyComponents;
